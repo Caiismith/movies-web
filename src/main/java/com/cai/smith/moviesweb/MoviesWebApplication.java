c421677f6bc12f2362d96e0df139ac7eff2b1448
Caiismith <caismith@outlook.com>
2019-05-17 23:25:07 +0100

    Initial commit

---
+++ src/main/java/com/cai/smith/moviesweb/MoviesWebApplication.java
@@ -0,0 +1,13 @@
+package com.cai.smith.moviesweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoviesWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoviesWebApplication.class, args);
+	}
+
+}
