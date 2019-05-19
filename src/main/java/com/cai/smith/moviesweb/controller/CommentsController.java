c9949dd2f31b99fb14405f10eec5314c26bc1908
Caiismith <caismith@outlook.com>
2019-05-19 19:31:00 +0100

    Add Most active user functonality

--- src/main/java/com/cai/smith/moviesweb/controller/CommentsController.java
+++ src/main/java/com/cai/smith/moviesweb/controller/CommentsController.java
@@ -1,4 +1,41 @@
 package com.cai.smith.moviesweb.controller;
 
+import com.cai.smith.moviesweb.model.Comment;
+import com.cai.smith.moviesweb.service.CommentsService;
+import com.cai.smith.moviesweb.service.EpochDateService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("comments")
 public class CommentsController {
+
+    @Autowired
+    private CommentsService commentsService;
+
+    @Autowired
+    private EpochDateService epochDateService;
+
+    private static final String COMMENTS_MODEL_ATTR_NAME = "comments";
+
+    @GetMapping("most-active")
+    public String getMostActive(Model model) {
+
+        List<Comment> comments = commentsService.getMostActive();
+
+        for(Comment comment: comments) {
+            Long seconds = Long.parseLong(comment.getDateCreated());
+
+            comment.setDateHumanReadable(epochDateService.convertToReadable(seconds));
+        }
+
+        model.addAttribute(COMMENTS_MODEL_ATTR_NAME, comments);
+
+        return "mostActive";
+    }
 }
--- src/main/java/com/cai/smith/moviesweb/service/CommentsService.java
+++ src/main/java/com/cai/smith/moviesweb/service/CommentsService.java
@@ -1,4 +1,31 @@
 package com.cai.smith.moviesweb.service;
 
+import com.cai.smith.moviesweb.model.Comment;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
+
+@Service
 public class CommentsService {
+
+    private RestTemplate restTemplate = new RestTemplate();
+
+    public List<Comment> getMostActive() {
+        ResponseEntity<List<Comment>> response = getApiList(
+                "http://localhost:8080/comments/most-active", HttpMethod.GET);
+        return response.getBody();
+    }
+
+    private ResponseEntity<List<Comment>> getApiList(String url, HttpMethod method) {
+        return restTemplate.exchange(
+                url,
+                method,
+                null,
+                new ParameterizedTypeReference<List<Comment>>() {}
+        );
+    }
 }
